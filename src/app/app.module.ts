c36e52c981c2842bf54f2301ac848bc1f4001592
Yash <you@example.com>
2023-05-23 18:37:57 +0530

    angular-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserAuthModule } from './user-auth/user-auth.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-auth/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  getData(data:NgForm){
+    console.log(data)
+  }
+}
